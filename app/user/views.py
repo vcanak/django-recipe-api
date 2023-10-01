2555f3ed96ab2c05d255940e144c2d1ddaefd905
Vedat Can AK <vcan.ak@gmail.com>
2023-10-01 21:01:09 +0300

    User api fix

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render # noqa
+"""
+Views for the user API.
+"""
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializers import UserSerializers, AuthTokenSerializers
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    """Create new user in the system."""
+    serializer_class = UserSerializers
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create new auth token for user."""
+    serializer_class = AuthTokenSerializers
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user in the system."""
+    serializer_class = UserSerializers
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
